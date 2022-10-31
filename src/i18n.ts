f4d495b3e640f646eb16ba6c77de4a33dfdb405b
Erik Escoffier <erik@satellitestud.io>
2022-10-31 21:05:33 +0100

    fixed issue with i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -24,7 +24,6 @@ i18n
     fallbackLng: 'en',
     debug: true,
     supportedLngs: ['en', 'fr'],
-    load: 'languageOnly',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
