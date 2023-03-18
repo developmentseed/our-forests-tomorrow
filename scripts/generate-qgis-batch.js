125cac84b0717676dcd87a8ea90273dc83079a42
Erik Escoffier <me@erik.nz>
2023-03-18 16:55:49 +0100

    Geosdescriber test

--- scripts/generate-qgis-batch.js
+++ scripts/generate-qgis-batch.js
@@ -18,22 +18,23 @@ const speciesList = JSON.parse(fs.readFileSync(SPECIES, 'utf-8'))
 
 const batchMain = speciesList.flatMap((species) => {
   if (!SPECIES_WHITELIST.includes(species)) return []
-  return GRIDS.flatMap((grid) => {
-    return RCPS.map((rcp) => ({
-      PARAMETERS: {
-        curbinnat: `'${EUTREES4F_DATASET_ABS_PATH}/bin/${species}/${species}_ens-sdms_cur_bin_nat.tif'`,
-        fut1probpot: `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp${rcp}_fut1_prob_pot.tif'`,
-        fut2probpot: `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp${rcp}_fut2_prob_pot.tif'`,
-        'fut2probpot (2)': `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp${rcp}_fut3_prob_pot.tif'`,
-        grid: `'/Users/erik/Work/eu-trees4f-viz/in/grid${grid.res}.geojson'`,
-        hex_grid: `'/Users/erik/Work/eu-trees4f-viz/in/grid${grid.res}_hex.geojson'`,
-      },
-      OUTPUTS: {
-        'native:reprojectlayer_1:final': `${GEOJSON_ABS_PATH}/species/${species}_rcp${rcp}_${grid.res.toString()}.geojson`,
-        'native:joinattributestable_1:test': `${GEOJSON_ABS_PATH}/species/${species}_rcp${rcp}_${grid.res.toString()}_hex.geojson`,
-      },
-    }))
-  })
+  return GRIDS.flatMap((grid) => ({
+    "PARAMETERS": {
+      "curbinnat": `'${EUTREES4F_DATASET_ABS_PATH}/bin/${species}/${species}_ens-sdms_cur_bin_nat.tif'`,
+      "grid": `'/Users/erik/Work/eu-trees4f-viz/in/grid20.geojson'`,
+      "hex_grid": `'/Users/erik/Work/eu-trees4f-viz/in/grid20_hex.geojson'`,
+      "sdms_rcp45_fut1": `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp45_fut1_prob_pot.tif'`,
+      "sdms_rcp45_fut2": `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp45_fut2_prob_pot.tif'`,
+      "sdms_rcp45_fut3": `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp45_fut3_prob_pot.tif'`,
+      "sdms_rcp85_fut1": `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp85_fut1_prob_pot.tif'`,
+      "sdms_rcp85_fut2": `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp85_fut2_prob_pot.tif'`,
+      "sdms_rcp85_fut3": `'${EUTREES4F_DATASET_ABS_PATH}/prob/${species}_ens-sdms_rcp85_fut3_prob_pot.tif'`
+    },
+    "OUTPUTS": {
+      "native:joinattributestable_1:hex": `${GEOJSON_ABS_PATH}/species/${species}_${grid.res.toString()}_hex.geojson`,
+      "native:reprojectlayer_1:final": `${GEOJSON_ABS_PATH}/species/${species}_${grid.res.toString()}.geojson`
+    }
+  }))
 })
 
 fs.writeFileSync(QGIS_BATCH_MAIN, JSON.stringify(batchMain))
